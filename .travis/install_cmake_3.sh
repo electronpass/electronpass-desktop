1db7af74769beaa8cf12801a09b6677ec1cdf41e
Ziga Patacko Koderman <ziga.patacko@protonmail.com>
2017-03-22 20:57:59 +0000

    add missing DEPS_DIR variable to cmake installation

--- .travis/install_cmake_3.sh
+++ .travis/install_cmake_3.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+DEPS_DIR="${TRAVIS_BUILD_DIR}/deps"
+mkdir -p ${DEPS_DIR} && cd ${DEPS_DIR}
 
 if [[ "${TRAVIS_OS_NAME}" == "linux" ]]; then
     CMAKE_URL="https://cmake.org/files/v3.7/cmake-3.7.2-Linux-x86_64.tar.gz"
