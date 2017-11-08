f99bbb0f3d9a90f8cf0371cce45de8a55b10fb42
Matej Marinko <matejmarinko123@gmail.com>
2017-11-08 21:00:24 +0100

    fix broken build after updating credentials script

---
+++ app/sync/keys.hpp
@@ -0,0 +1,12 @@
+#ifndef ELECTRONPASS_KEYS_HPP
+#define ELECTRONPASS_KEYS_HPP
+
+#define kGdriveClientID "@GDRIVE_ID@"
+#define kGdriveClientSecret "@GDRIVE_SECRET@"
+
+#define kDropboxClientID "@DROPBOX_ID@"
+#define kDropboxClientSecret "@DROPBOX_SECRET@"
+
+#define kTokenPassword "@TOKEN_PASSWORD@"
+
+#endif //ELECTRONPASS_KEYS_HPP
