70eca753436ebd16c05ca13fe236bd66fbef55cb
Matej Marinko <matejmarinko123@gmail.com>
2017-11-11 16:22:34 +0100

    Add filedialog title

--- app/file_dialog.cpp
+++ app/file_dialog.cpp
@@ -1,3 +1,20 @@
+/*
+This file is part of ElectronPass.
+
+ElectronPass is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+ElectronPass is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ElectronPass. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "file_dialog.hpp"
 
 #include <iostream>
@@ -32,6 +49,7 @@ QUrl FileDialog::open_file() {
     dialog.setAcceptMode(QFileDialog::AcceptOpen);
     dialog.setDirectory(QDir::homePath());
     dialog.setFileMode(QFileDialog::ExistingFile);
+    dialog.setWindowTitle(QString("Select wallet file"));
 
     if (dialog.exec() && dialog.selectedFiles().size() > 0) {
         QUrl selected_file = dialog.selectedFiles()[0];
