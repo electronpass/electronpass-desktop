75f5924432926c2c900ddde1442de18d44ffffe6
Ziga Patacko Koderman <ziga.patacko@protonmail.com>
2018-01-27 13:39:21 +0100

    separate appdir build script

---
+++ create-linux-appdir.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+file=appdir/usr/local
+root_path=$(pwd)
+if [ ! -e "$file" ]
+then
+  mkdir -p $file
+  mkdir -p $file/share/applications
+  mkdir -p $file/bin
+fi
+
+cp bin/electronpass $file/bin
+cp data/electronpass.desktop $file/share/applications
+cp data/electronpass.svg $file/share/applications
+cd $file/bin
+if [ ! -e "linuxdeployqt-continuous-x86_64.AppImage" ]
+then
+  wget "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
+  chmod a+x linuxdeployqt-continuous-x86_64.AppImage
+fi
+
+unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
+export VERSION=$(git rev-parse --short HEAD) # linuxdeployqt uses this for naming the file
+./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/local/share/applications/*.desktop -bundle-non-qt-libs -qmldir=$root_path/app/qml/
